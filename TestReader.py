d4502b7bea630fd1af3c26a025223fa7801a3d6d
ryoungster <ryoungster@comcast.net>
2019-05-18 11:52:39 -0700

    Branching

--- TestReader.py
+++ TestReader.py
@@ -1,16 +1,21 @@
-#for reading the test and anking questions
-file = open("TestQuestions.txt", "r")
-score = 0
-for i in range(4):
-    for i in range(4):
-        q1 = file.readline()
-        print(q1)
+option = input("TEST or CREATE questions:")
 
-    answer = input("Answer: ")
-    correct = file.readline()[0]
-    if answer == correct:
-        print("Correct")
-        score += 1
-    else:
-        print("Wrong, the answer was",correct)
-print("\nTest over\nYour score was",(score/4)*100,"%")
+if option == "Test":
+    #for reading the test and anking questions
+    file = open("TestQuestions.txt", "r")
+    score = 0
+    for i in range(4):
+        for i in range(4):
+            q1 = file.readline()
+            print(q1)
+    
+        answer = input("Answer: ")
+        correct = file.readline()[0]
+        if answer == correct:
+            print("Correct")
+            score += 1
+        else:
+            print("Wrong, the answer was",correct)
+    print("\nTest over\nYour score was",(score/4)*100,"%")
+elif option == "CREATE":
+    pass
