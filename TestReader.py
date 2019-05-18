65fbedbf109347d82623931f85e9520f9568c42a
Xavier <xavier@santiagoville.net>
2019-05-18 11:44:00 -0700

    Done!

--- TestReader.py
+++ TestReader.py
@@ -1,1 +1,16 @@
 #for reading the test and anking questions
+file = open("TestQuestions.txt", "r")
+score = 0
+for i in range(4):
+    for i in range(4):
+        q1 = file.readline()
+        print(q1)
+
+    answer = input("Answer: ")
+    correct = file.readline()[0]
+    if answer == correct:
+        print("Correct")
+        score += 1
+    else:
+        print("Wrong, the answer was",correct)
+print("\nTest over\nYour score was",(score/4)*100,"%")
